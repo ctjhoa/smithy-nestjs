f43729b581882f100b32c25e97d60bd794905952
Camille TJHOA <camille.tjhoa@outlook.com>
2025-03-14 18:10:58 +0100

    feat: add smithy api project

---
+++ smithy-api/README.md
@@ -0,0 +1,3 @@
+# smithy-api
+
+This library was generated with [Nx](https://nx.dev).
---
+++ smithy-api/src/index.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line @nx/enforce-module-boundaries
+export * from './lib/source/typescript-ssdk-codegen/src/index';
